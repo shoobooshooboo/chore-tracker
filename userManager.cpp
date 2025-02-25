9cc317c81035e968e212197d4bc5765a295498fc
ddsmith9254 <ddsmith9254@gmail.com>
2025-02-25 12:18:56 -0800

    began implementation of UserManager functions

---
+++ userManager.cpp
@@ -0,0 +1,30 @@
+#include "userManager.hpp"
+
+namespace UserManager {
+    bool isLoaded(const uint64_t userID) {
+        return findLoadedUser(userID) != loadedUsers.cend();
+    }
+
+    decltype(loadedUsers)::const_iterator findLoadedUser(const uint64_t userID) {
+        return std::ranges::find_if(loadedUsers, 
+            [userID](const User& loadedUser){ return userID == loadedUser.getID(); }, 
+            [](const std::shared_ptr<User>& ptr){ return *ptr.get();}
+        );
+    }
+
+    decltype(loadedUsers)::const_iterator loadUser(const uint64_t userID) {
+        auto it{findLoadedUser(userID)};
+
+        if (it == loadedUsers.cend()) 
+        {
+            // load the new user and push to the back
+            // if we decide to maintain ordering by ID in the file, do a binary search
+            // otherwise linear also would probably be fine
+            // use below to skip lines after parsing the ID token if not a match
+            // <filestream here>.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            it = std::prev(loadedUsers.cend());
+        } 
+
+        return it;
+    }
+}
--- userManager.hpp
+++ userManager.hpp
@@ -3,17 +3,19 @@
 #define USER_MANAGER_HPP
 
 #include <vector>
+#include <algorithm>
 #include <filesystem>
 #include "user.hpp"
 
 namespace UserManager {
     namespace {
-        const std::filesystem::path usersFile; 
+        constexpr std::filesystem::path usersFile; 
         
         std::vector<std::shared_ptr<User>> loadedUsers;
     }
     
     [[nodiscard]] bool isLoaded(const uint64_t userID);
+    [[nodiscard]] decltype(loadedUsers)::const_iterator findLoadedUser(const uint64_t userID);
     // returns iterator to the user, or end if no user matching ID was found
     decltype(loadedUsers)::const_iterator loadUser(const uint64_t userID);
     void storeUser(const User& user);
