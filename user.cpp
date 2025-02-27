ffc207b8aa1f5ab524b3fa3c74213b4e0b557bf9
ddsmith9254 <ddsmith9254@gmail.com>
2025-02-26 20:00:15 -0800

    defined setName

--- user.cpp
+++ user.cpp
@@ -15,3 +15,11 @@ const std::string& User::getName(void) const noexcept {
 uint64_t User::getID(void) const noexcept {
     return mUserID;
 }
+
+bool User::setName(const std::string_view name) {
+    if (std::ranges::find_if(name, [](const decltype(name)::value_type c){ return !(isalpha(c)); }) != name.cend()) {
+        return false;
+    }
+    mName = name;
+    return true;
+}
