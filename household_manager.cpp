5e3ff5d2a209812f868b0e63fd4441fe9d965dd1
ddsmith9254 <ddsmith9254@gmail.com>
2025-02-26 18:30:59 -0800

    added household manager to interface with file stored household information

---
+++ household_manager.cpp
@@ -0,0 +1,24 @@
+#include "household_manager.hpp"
+
+std::shared_ptr<Household> HouseholdManager::loadHousehold(const uint64_t householdID) {
+    Household house;
+
+    // TODO read all data into household from file
+
+    // TODO read the userIDs for this household from file and plug into below
+
+    auto householdPtr{ std::make_shared<Household>(house) };
+
+    while (true /*TODO actual condition should be while users left from this household*/) {
+        uint64_t userID{/*TODO should have value from file*/};
+
+        auto user{ UserManager::loadUser(userID) };
+
+        householdPtr->handleUserJoining(std::weak_ptr(*user));
+        (*user)->addHousehold(householdPtr);
+    }
+
+
+
+    return householdPtr;
+}
---
+++ household_manager.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef HOUSEHOLD_MANAGER_HPP
+#define HOUSEHOLD_MANAGER_HPP
+
+#include <vector>
+#include <algorithm>
+#include <limits>
+#include <filesystem>
+#include <memory>
+#include "user.hpp"
+#include "household.hpp"
+#include "user_manager.hpp"
+
+namespace HouseholdManager {
+    namespace {
+        constexpr std::filesystem::path householdsFile; 
+
+        // no owning vector in namespace; ownership is solely through users.
+    }
+
+    // loads a household from file
+    // ONLY USE TO GET THE FIRST INSTANCE OF THE HOUSEHOLD! 
+    // return value is owning pointer of the household, with all users loaded
+    std::shared_ptr<Household> loadHousehold(const uint64_t householdID); 
+
+}  
+
+
+#endif
