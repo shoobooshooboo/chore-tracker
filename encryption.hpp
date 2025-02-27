00f12b5ef045a9580fc105d13b40ca6cb0331360
ddsmith9254 <ddsmith9254@gmail.com>
2025-02-26 21:48:37 -0800

    enclosed functions in Encryption namespace

--- encryption.hpp
+++ encryption.hpp
@@ -1,10 +1,17 @@
 #pragma once
+#ifndef ENCRYPTION_HPP
+#define ENCRYPTION_HPP
+
 #include <filesystem>
 #include <string>
 #include <fstream>
 
-std::string encrypt(std::string name, std::string password);
-std::string decrypt(std::string name, std::string password);
+namespace Encryption{
+    std::string encrypt(std::string name, std::string password);
+    std::string decrypt(std::string name, std::string password);
 
-void encryptFile();
-void decryptFile();
+    void encryptFile();
+    void decryptFile();
+}
+
+#endif
