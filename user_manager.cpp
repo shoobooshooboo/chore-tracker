30cc07c7e311711a5f8be1ed1b55e03a9e82ffa2
ddsmith9254 <ddsmith9254@gmail.com>
2025-02-26 19:11:00 -0800

    added some more framework for the loading user process

--- user_manager.cpp
+++ user_manager.cpp
@@ -14,12 +14,16 @@ namespace UserManager {
 
         if (it == loadedUsers.cend()) 
         {
+            User userToLoad;
             // TODO 
             // load the new user and push to the back
             // if we decide to maintain ordering by ID in the file, do a binary search
             // otherwise linear also would probably be fine
             // use below to skip lines after parsing the ID token if not a match
             // <filestream here>.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+            loadedUsers.push_back(std::make_shared<User>(userToLoad));
+            
             it = std::prev(loadedUsers.cend());
         } 
 
