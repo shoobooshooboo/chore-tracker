e2561a7ee7eae0901474cafc6af8f742c98c1df9
ddsmith9254 <ddsmith9254@gmail.com>
2025-03-02 12:55:10 -0800

    made a main.cpp for use in demo vid

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "app.hpp"
+
+
+// DEMO MAIN
+// All of the user IO here to be replaced with actual UI interaction
+int main(void) {
+
+    char input{'\0'};
+    while (input < '1' || input > '2') {
+        std::cout << "1. Log In\n2. Sign Up\n";
+        std::cin >> input;
+    }
+    switch (input) {
+        case '1':
+            App::logIn();
+            break;
+        case '2':
+            App::signUp();
+            break;
+    }
+    
+    assert(UserManager::loadedUsers.size() == 1);
+
+    App::startup();
+
+
+}
