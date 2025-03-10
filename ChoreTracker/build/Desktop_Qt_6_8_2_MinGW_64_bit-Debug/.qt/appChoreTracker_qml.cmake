6a1965231ad31e4b7a88ec607ff533e8f62eafdc
Noel vanSchaick <johnonymous4@gmail.com>
2025-03-10 13:24:05 -0700

    initial frontend commit

---
+++ ChoreTracker/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/.qt/appChoreTracker_qml.cmake
@@ -0,0 +1,9 @@
+
+set(target "appChoreTracker")
+set(working_dir "C:/Desktop/code/Personal/chore-tracker/ChoreTracker")
+set(src_and_dest_list
+    "C:/Desktop/code/Personal/chore-tracker/ChoreTracker/Main.qml"
+    "C:/Desktop/code/Personal/chore-tracker/ChoreTracker/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/ChoreTracker/Main.qml"
+
+)
+set(timestamp_file "C:/Desktop/code/Personal/chore-tracker/ChoreTracker/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/.qt/appChoreTracker_qml.txt")
